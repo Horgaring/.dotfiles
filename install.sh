93bee474b1941a63ffaa71c97a8e0e3be5e2242a
Horgaring <dimarastvorcev6123@gmail.com>
2026-04-13 16:53:01 +0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+
+HYPRPAPER=~/.config/hypr/hyprpaper.conf 
+WALLPAPER=~/Pictures/Wallpapers/wallpaper.jpeg
+MONITORS=$(hyprctl monitors -j | jq -r '.[] | .name' 2>/dev/null || echo "")
+
+# 2. Create Directories
+mkdir -p ~/.config/hypr
+mkdir -p ~/Pictures/Wallpapers
+
+# 3. Copy Static Files
+cp ./.config/hyprland.conf ~/.config/hypr/hyprland.conf 
+cp ./.vimrc ~/.vimrc
+cp -r ./waybar ~/.config/
+cp -p ./Wallpapers/wallpaper.jpeg "$WALLPAPER"
+
+# 4. Generate Hyprpaper Config
+echo "preload = $WALLPAPER" > "$HYPRPAPER"
+
+if [ -z "$MONITORS" ]; then
+	cat <<EOF >> "$HYPRPAPER"
+wallpaper {
+    monitor = 
+    path = $WALLPAPER
+    fit_mode = cover
+}
+EOF
+else
+	for MON in $MONITORS; do
+		cat <<EOF >> "$HYPRPAPER"
+wallpaper {
+    monitor = $MON
+    path = $WALLPAPER
+    fit_mode = cover
+}
+EOF
+done
+fi
+
+echo "splash = false" >> "$HYPRPAPER"
+
+echo "Done!"
+
